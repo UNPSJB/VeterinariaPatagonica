ac418e714e4e8c5ade0c320eaf9c699535bc9595
mati <matias.ducid08@gmail.com>
2018-09-12 01:48:56 -0300

    HTML Base y de Clientes Creados

--- VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeClientes/views.py
+++ VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeClientes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def base(request):
+    context = {}
+    template = loader.get_template('GestionDeClientes/sitio_base.html')
+    return HttpResponse(template.render(context, request))
+
+def clientes(request):
+    context = {}
+    template = loader.get_template('GestionDeClientes/GestionDeClientes.html')
+    return HttpResponse(template.render(context, request))
--- VeterinariaPatagonica/VeterinariaPatagonica/urls.py
+++ VeterinariaPatagonica/VeterinariaPatagonica/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import url
+from VeterinariaPatagonica.Apps.GestionDeClientes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^$',views.base, name='base'),
+    url(r'GestionDeClientes', views.clientes, name='clientes'),
 ]
