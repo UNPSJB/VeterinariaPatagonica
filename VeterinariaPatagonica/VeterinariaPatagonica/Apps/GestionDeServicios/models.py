b63568d0b2cf12b0af6a36438eae07c2974488f3
fernando <feersg172@gmail.com>
2018-09-15 19:45:22 -0300

    cambio en el modelo de servicio

--- VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeServicios/models.py
+++ VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeServicios/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 from VeterinariaPatagonica.Apps.GestionDeInsumos.models import Insumo
 from django.views.generic.list import ListView
-# Create your models here.
-class ListaCantidades(ListView):
-    l_cantidadInsumos = models.PositiveSmallIntegerField()
 
-class ListaInsumos(ListView):
-    l_insumos = Insumo
+# Create your models here.
+class CantidadList(ListView):
+    cantidadInsumos = models.PositiveSmallIntegerField()
+
+
+class InsumosList(ListView):
+    insumo = models.ForeignKey(Insumo, null=False, blank=False)
+
 
 class Servicio(models.Model):
     t_tipo = (('C','Consulta'),('Q','Quirurgica'))
     tipo = models.CharField(max_length=25, choices=t_tipo, default='Tipo de Práctica aqui')
-    nombre = models.CharField(max_length=50, null=False, blank=False,help_text="Ingresa el nombre del Servicio")
+    nombre = models.CharField(max_length=50, null=False, blank=False, help_text='Ingresa el nombre del Servicio')
     descripcion = models.CharField(max_length=200)
-    tiempoEstimado = models.TimeField(auto_now_add=False,help_text="Tiempo de Duración del Servicio")
+    insumos = InsumosList
+    cantidadInsumos = CantidadList
+    tiempoEstimado = models.TimeField(auto_now=False, help_text='Tiempo de Duración del Servicio')
     precioManoDeObra = models.PositiveSmallIntegerField()
-    insumos = ListaInsumos
-    cantidadInsumos = ListaCantidades
+
 
 
     def __str__(self):
