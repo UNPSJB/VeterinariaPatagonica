2b8f761ce06e00b7a2f37e2088c34e02fc8e4850
fernando <feersg172@gmail.com>
2018-09-15 20:16:45 -0300

    cambio en el modelo de servicio

--- VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeServicios/models.py
+++ VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeServicios/models.py
@@ -3,13 +3,13 @@ from VeterinariaPatagonica.Apps.GestionDeInsumos.models import Insumo
 from django.views.generic.list import ListView
 
 # Create your models here.
-class CantidadList(ListView):
-    cantidadInsumos = models.PositiveSmallIntegerField()
+#class CantidadList(ListView):
+#   cantidadInsumos = models.PositiveSmallIntegerField()
 
 
 class InsumosList(ListView):
-    insumo = models.ForeignKey(Insumo, null=False, blank=False)
-
+    insumo = models.ForeignKey(Insumo, null=False, blank=False, on_delete=models.CASCADE)
+    cantidadInsumo = models.PositiveSmallIntegerField
 
 class Servicio(models.Model):
     t_tipo = (('C','Consulta'),('Q','Quirurgica'))
@@ -17,10 +17,9 @@ class Servicio(models.Model):
     nombre = models.CharField(max_length=50, null=False, blank=False, help_text='Ingresa el nombre del Servicio')
     descripcion = models.CharField(max_length=200)
     insumos = InsumosList
-    cantidadInsumos = CantidadList
+    #cantidadInsumos = CantidadList
     tiempoEstimado = models.TimeField(auto_now=False, help_text='Tiempo de Duración del Servicio')
     precioManoDeObra = models.PositiveSmallIntegerField()
-
 
 
     def __str__(self):
