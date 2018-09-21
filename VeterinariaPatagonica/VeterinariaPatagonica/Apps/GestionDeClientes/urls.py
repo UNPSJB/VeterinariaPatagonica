94e28b1f1db07dab0be050c3eacd72366a0b3181
mati <matias.ducid08@gmail.com>
2018-09-21 00:43:16 -0300

    formulario cliente andando

--- VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeClientes/urls.py
+++ VeterinariaPatagonica/VeterinariaPatagonica/Apps/GestionDeClientes/urls.py
@@ -4,7 +4,7 @@ from VeterinariaPatagonica import views
 
 app_name = 'clientes'
 urlpatterns = [
-    url(r'$',clientes_views.clientes, name="cliente"),
-    url(r'altacliente.html/$',views.verdemo, name="alta"),
+    url(r'^$',clientes_views.clientes, name="cliente"),
+    url(r'altacliente.html$',clientes_views.alta, name="alta"),
     #url(r'altacliente.html/$',clientes_views.alta, name='alta'),
 ]
