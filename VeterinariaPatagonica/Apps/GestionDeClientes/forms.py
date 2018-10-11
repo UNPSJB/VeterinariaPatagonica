19d2a996a6a734d9f1f105d6981d87c2f23ef3fc
jony <jntorres2014@gmail.com>
2018-10-11 18:24:46 -0300

    agregue formulario de mascota

--- VeterinariaPatagonica/Apps/GestionDeClientes/forms.py
+++ VeterinariaPatagonica/Apps/GestionDeClientes/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Cliente
+from django.core.validators import RegexValidator
 
 '''class creacionModelForm(forms.ModelForm):
     class meta:
--- VeterinariaPatagonica/Apps/GestionDeMascotas/urls.py
+++ VeterinariaPatagonica/Apps/GestionDeMascotas/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import url
 from . import views as mascotas_views
-from VeterinariaPatagonica import views
-
+#from VeterinariaPatagonica import views
+from django.urls import path
+from . import views
 app_name = 'mascotas'
 
 urlpatterns = [
     url(r'^$', mascotas_views.mascota, name='mascota'),
     url(r'altamascota.html/$', mascotas_views.alta, name='alta'),
     # url(r'altamascota.html/$',views.verdemo, name='alta'),
+
+    path('ver/<int:id>/', views.ver, name='mascotasVer'),
+    path('modificar/<int:id>/', views.modificar, name='mascotasModificar'),
+    path('deshabilitar/<int:id>/', views.deshabilitar, name='mascotasDeshabilitar'),
+    path('habilitar/<int:id>/', views.habilitar, name='mascotasHabilitar'),
+    path('eliminar/<int:id>/', views.eliminar, name='mascotasEliminar'),
+    path('ver_habilitados/', views.verHabilitados, name='mascotasVerHabilitados'),
+    path('ver_deshabilitados/', views.verDeshabilitados, name='mascotasVerDeshabilitados'),
+    path('', views.verHabilitados, name='mascotasVerHabilitados'),
+
+
 ]
