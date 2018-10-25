fdf6602ee737cbbbdbbd69e966b62e52243586de
LucilaAllende <abigail_lucii@hotmail.com>
2018-10-25 18:14:49 -0300

    Actualizando migraciones

--- VeterinariaPatagonica/dumpdata.sh
+++ VeterinariaPatagonica/dumpdata.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 echo "Dumpdataeando auth"
 python3 manage.py dumpdata auth > ./fixtures/auth.json
-for f in GestionDeInsumos GestionDeClientes \
-GestionDeMascotas GestionDePracticas \
-GestionDeServicios GestionDeTiposDeAtencion
+for f in GestionDeInsumos GestionDeClientes GestionDeMascotas GestionDeTiposDeAtencion GestionDeServicios GestionDePracticas
+
 do
-  echo "Dumpdataeando $f"
-  mkdir -p ./Apps/$f/fixtures
-  python3 manage.py dumpdata $f > ./Apps/$f/fixtures/$f.json
+echo "Dumpdataeando $f"
+mkdir -p ./Apps/$f/fixtures
+python3 manage.py dumpdata $f > ./Apps/$f/fixtures/$f.json
 done
