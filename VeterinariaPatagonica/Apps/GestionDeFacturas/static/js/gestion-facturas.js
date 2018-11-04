629d2d90baeecc12e0a097b9220f7e292c968288
jony <jntorres2014@gmail.com>
2018-11-04 10:21:26 -0300

    commit para detalles de productos en facturas

--- VeterinariaPatagonica/Apps/GestionDeFacturas/static/js/gestion-facturas.js
+++ VeterinariaPatagonica/Apps/GestionDeFacturas/static/js/gestion-facturas.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
     $('.facturas-form-linkinfo').each(function(i,x){
@@ -7,4 +8,33 @@ $(document).ready(function() {
             $('#'+a).toggle();
         });
     });
+});
+
+
+$(function() {
+	let total = Number($("#id_form-TOTAL_FORMS").val());
+	let initial = $("#id_form-INITIAL_FORMS").val();
+	let min = Number($("#id_form-MIN_NUM_FORMS").val());
+	let max = Number($("#id_form-MAX_NUM_FORMS").val());
+
+	let $buttons = $("#buttons");
+	let template = $("#formset-template").html().replace(RegExp(`form-${initial}`,"g"), `form-ITEM`);
+	$("#formset-template").remove();
+	total = total - 1;
+	$("#id_form-TOTAL_FORMS").val(total);
+
+	let add = function() {
+		if (total < max) {
+			let field = template.replace(RegExp(`form-ITEM`,"g"), `form-${total}`);
+			field = `<div class="col-xs-9" id="item-${total}">${field}</div>`;
+			total += 1;
+			$("#id_form-TOTAL_FORMS").val(total);
+			$buttons.before(field);
+		}
+	}
+
+
+	$(document).on("click",".rm",remove);
+	$("#button-add").click(add);
+
 });
