425a149ab60e9b3db189c4b27440b55217c3f155
LucilaAllende <abigail_lucii@hotmail.com>
2018-11-09 16:22:28 -0300

    Volvi a cargar los json

--- VeterinariaPatagonica/loaddata.cmd
+++ VeterinariaPatagonica/loaddata.cmd
@@ -1,3 +1,6 @@
+echo "Load Gestion de Rubros"
+python manage.py loaddata ./Apps/GestionDeRubros/fixtures/GestionDeRubros.json
+
 echo "Load Gestion de Insumos"
 python manage.py loaddata ./Apps/GestionDeProductos/fixtures/GestionDeProductos.json
 
@@ -7,13 +10,11 @@ python manage.py loaddata ./Apps/GestionDeClientes/fixtures/GestionDeClientes.js
 echo "Load Gestion de Mascotas"
 python manage.py loaddata ./Apps/GestionDeMascotas/fixtures/GestionDeMascotas.json
 
-echo "Load Gestion de Practicas"
-python manage.py loaddata ./Apps/GestionDePracticas/fixtures/GestionDePracticas.json
-
 echo "Load Gestion de Servicios"
 python manage.py loaddata ./Apps/GestionDeServicios/fixtures/GestionDeServicios.json
 
 echo "Load Gestion de TiposDeAtencion"
 python manage.py loaddata ./Apps/GestionDeTiposDeAtencion/fixtures/GestionDeTiposDeAtencion.json
 
-python manage.py loaddata ./Apps/GestionDeRubros/fixtures/GestionDeRubros.json
+echo "Load Gestion de Practicas"
+python manage.py loaddata ./Apps/GestionDePracticas/fixtures/GestionDePracticas.json
