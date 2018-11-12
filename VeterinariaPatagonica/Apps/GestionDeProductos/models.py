f019994e9916ca8e196b160e621ba3967df5203b
fernando <feersg172@gmail.com>
2018-11-12 04:01:31 -0300

    Productos: Realizacion de Validaciones en los campos Stock, Precio por Unidad y Precio de Compra, para que no se admita valores negativos.

--- VeterinariaPatagonica/Apps/GestionDeProductos/models.py
+++ VeterinariaPatagonica/Apps/GestionDeProductos/models.py
@@ -1,5 +1,6 @@
+from decimal import Decimal
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from Apps.GestionDeRubros import models as grmodels
 from VeterinariaPatagonica import tools
 
@@ -22,11 +23,15 @@ class Producto (models.Model):
     MAX_NOMBRE = 50
     REGEX_NOMBRE = '^[0-9a-zA-Z-_ .]{3,100}$'
     REGEX_NUMERO = '^[0-9]{1,12}$'
-    MAX_DIGITOS = 7
-    MAX_DECIMALES = 2
     MAXDESCRIPCION= 150
 
-
+    STOCK_MIN_VALUE = 1
+    STOCK_MAX_VALUE = 99
+    MAX_ENTERO = 4
+    MAX_DECIMAL = 2
+    MIN_PRECIO = Decimal(0)
+    MAX_PRECIO = Decimal(9000.00)
+    PRECIO = MAX_ENTERO + MAX_DECIMAL
 
     NONE = 0
     GRAMO = 1
@@ -104,14 +109,10 @@ class Producto (models.Model):
 
     stock = models.IntegerField(
         help_text="Stock del Producto",
-        #max_digits = MAX_DIGITOS,
-        #unique = False,
-        #null = True,
-        #blank = True,
-        #validators = [RegexValidator(regex=REGEX_NUMERO)],
-        #error_messages = {
-            #'max_digits' : "El stock puede tener a lo sumo {} digitos".format(MAX_DIGITOS),
-         #   }
+        validators=[
+            MinValueValidator(STOCK_MIN_VALUE, message=("El Stock debe ser mayor a 0")),
+            MaxValueValidator(STOCK_MAX_VALUE, message=("El Stock no puede ser mayor a {}").format(STOCK_MAX_VALUE))
+        ]
     )
 
     formaDePresentacion = models.PositiveSmallIntegerField(
@@ -128,17 +129,26 @@ class Producto (models.Model):
 
     precioPorUnidad = models.DecimalField(
         help_text="Precio del Producto",
-        max_digits = MAX_DIGITOS,
-        decimal_places = MAX_DECIMALES
+        max_digits=PRECIO,
+        decimal_places=MAX_DECIMAL,
+        validators=[
+            MinValueValidator(MIN_PRECIO, message=("El precio no debe ser menor a ${:.%df}" % (MAX_DECIMAL)).format(MIN_PRECIO)),
+            MaxValueValidator(MAX_PRECIO, message=("El precio no puede ser mayor a ${:.%df}" % (MAX_DECIMAL)).format(MAX_PRECIO)),
+        ]
     )
+
 
     precioDeCompra = models.DecimalField(
         help_text="Precio de Compra del Producto",
-        max_digits = MAX_DIGITOS,
+        max_digits = PRECIO,
         unique=False,
         null=False,
         blank=False,
-        decimal_places = MAX_DECIMALES
+        decimal_places = MAX_DECIMAL,
+        validators=[
+            MinValueValidator(MIN_PRECIO, message=("El precio no debe ser menor a ${:.%df}" % (MAX_DECIMAL)).format(MIN_PRECIO)),
+            MaxValueValidator(MAX_PRECIO, message=("El precio no puede ser mayor a ${:.%df}" % (MAX_DECIMAL)).format(MAX_PRECIO)),
+        ],
     )
 
 
@@ -154,7 +164,6 @@ class Producto (models.Model):
         }
     )
 
-
     descripcion = models.TextField(
         help_text="Descripcion del Rubro",
         max_length=MAXDESCRIPCION,
@@ -168,6 +177,7 @@ class Producto (models.Model):
     )
 
     baja = models.BooleanField(default=False)
+
 
     objects = ProductoManager()
 
