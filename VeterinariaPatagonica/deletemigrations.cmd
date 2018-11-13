73b79aca0dc4ed16e28976e5f69abb1ac22b6367
LucilaAllende <abigail_lucii@hotmail.com>
2018-11-13 12:30:07 -0300

    Modificando el script

--- VeterinariaPatagonica/deletemigrations.cmd
+++ VeterinariaPatagonica/deletemigrations.cmd
@@ -1,32 +1,31 @@
-rmdir ./Apps/GestionDeProductos/migrations
-mkdir ./Apps/GestionDeProductos/migrations
-notepad ./Apps/GestionDeProductos/migrations/__init__.py
+rmdir .\Apps\GestionDeClientes\migrations
+mkdir .\Apps\GestionDeClientes\migrations
+notepad .\Apps\GestionDeClientes\migrations\__init__.py
 
-rmdir ./Apps/GestionDeClientes/migrations
-mkdir ./Apps/GestionDeClientes/migrations
-notepad ./Apps/GestionDeClientes/migrations/__init__.py
+rmdir .\Apps\GestionDeFacturas\migrations
+mkdir .\Apps\GestionDeFacturas\migrations
+notepad .\Apps\GestionDeFacturas\migrations\__init__.py
 
-rmdir ./Apps/GestionDeMascotas/migrations
-mkdir ./Apps/GestionDeMascotas/migrations
-notepad ./Apps/GestionDeMascotas/migrations/__init__.py
+rmdir .\Apps\GestionDeMascotas\migrations
+mkdir .\Apps\GestionDeMascotas\migrations
+notepad .\Apps\GestionDeMascotas\migrations/__init__.py
 
-rmdir ./Apps/GestionDePracticas/migrations
-mkdir ./Apps/GestionDePracticas/migrations
-notepad ./Apps/GestionDePracticas/migrations/__init__.py
+rmdir .\Apps\GestionDePracticas\migrations
+mkdir .\Apps\GestionDePracticas\migrations
+notepad .\Apps\GestionDePracticas\migrations\__init__.py
 
-rmdir ./Apps/GestionDeServicios/migrations
-mkdir ./Apps/GestionDeServicios/migrations
-notepad ./Apps/GestionDeServicios/migrations/__init__.py
+rmdir .\Apps\GestionDeProductos\migrations
+mkdir .\Apps\GestionDeProductos\migrations
+notepad .\Apps\GestionDeProductos\migrations\__init__.py
 
-rmdir ./Apps/GestionDeTiposDeAtencion/migrations
-mkdir ./Apps/GestionDeTiposDeAtencion/migrations
-notepad ./Apps/GestionDeTiposDeAtencion/migrations/__init__.py
+rmdir .\Apps\GestionDeRubros\migrations
+mkdir .\Apps\GestionDeRubros\migrations
+notepad .\Apps\GestionDeRubros\migrations\__init__.py
 
-rmdir ./Apps/GestionDeRubros/migrations
-mkdir ./Apps/GestionDeRubros/migrations
-notepad ./Apps/GestionDeRubros/migrations/__init__.py
+rmdir .\Apps\GestionDeServicios\migrations
+mkdir .\Apps\GestionDeServicios\migrations
+notepad .\Apps\GestionDeServicios\migrations\__init__.py
 
-rmdir ./Apps/GestionDeFacturas/migrations
-mkdir ./Apps/GestionDeFacturas/migrations
-notepad ./Apps/GestionDeFacturas/migrations/__init__.py
-
+rmdir .\Apps\GestionDeTiposDeAtencion\migrations
+mkdir .\Apps\GestionDeTiposDeAtencion\migrations
+notepad .\Apps\GestionDeTiposDeAtencion\migrations\__init__.py
