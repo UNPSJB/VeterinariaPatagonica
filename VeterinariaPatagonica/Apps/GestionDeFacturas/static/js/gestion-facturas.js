b6f0bbfc2a96d26cc4786d6d238c1ea2ab952bb2
mati <matias.ducid08@gmail.com>
2018-11-17 19:00:47 -0300

    calcular total recalcula al cambiar el checkbox eliminar, mostrando realmente lo que se va a facturar

--- VeterinariaPatagonica/Apps/GestionDeFacturas/static/js/gestion-facturas.js
+++ VeterinariaPatagonica/Apps/GestionDeFacturas/static/js/gestion-facturas.js
@@ -11,19 +11,15 @@
     //console.log(template);
     let iterador = 0;
     for (iterador ; iterador<totalTuplas; iterador++){
-      let cantidad =$(`#id_form-${iterador}-cantidad`).on("input", function() { obtenerTotal(); });//Obtengo la cantidad del elemento recien agregado
+      let cantidad =$(`#id_form-${iterador}-cantidad`).on("input", function() { calcularTotal(); });//Obtengo la cantidad del elemento recien agregado
       let producto = document.querySelector(`select[name="form-${iterador}-producto"]`);
       producto.onchange=function(e) {
-        obtenerTotal();
+        calcularTotal();
       };
+      let borrar = $(`#id_form-${iterador}-DELETE`).on("change", function() { calcularTotal(); });
     }
 
-    let mostrar = function(){
-      console.log("Imprimo desde la función");
-    }
-
-
-    let obtenerTotal = function(){
+    let calcularTotal = function(){
       let i=0;
       console.log("OBTENER TOTAL - entrando a la función.");
       let acumulador =0;
@@ -32,27 +28,30 @@
         let $item = $(item);
         let inputCantidad =$(`#id_form-${i}-cantidad`, $item[0]);
         let inputSelect = $(`#id_form-${i}-producto`,$item[0]);
-        let $inputSelect = $(inputSelect)
+        let inputDelete = $(`#id_form-${i}-DELETE`, $item[0]);
+        let $inputSelect = $(inputSelect);
+        let borrar = inputDelete[0];
         let cantidad = inputCantidad[0].value;
         let texto = $inputSelect.find(":selected").text();
-
-        if (cantidad < 0){ cantidad = 0;}
-
-        if ((texto != "---------") && (cantidad)) {
-          let varParseo = $inputSelect.find(":selected").text();
-          varParseo = varParseo.split(",");
-          varParseo = varParseo[2];
-          varParseo = varParseo.split(" ");
-          varParseo = varParseo[2];
-          let precioProducto = parseInt(varParseo);
-          let precioFinalTupla = precioProducto * cantidad
-          console.log("El precio de la tupla completa es: %s",precioFinalTupla);
-          let inputTotal=$(`#id_total`);
-          let precioTotal = parseInt(inputTotal[0].value);
-          acumulador +=precioFinalTupla;
-          console.log(precioTotal);
-        }else{
-          console.log("No se puede calcular el precio, falta llenar algún campo.");
+        //console.log(borrar.checked);
+        if (borrar.checked == false){
+          if (cantidad < 0){ cantidad = 0;}
+          if ((texto != "---------") && (cantidad)) {
+            let varParseo = $inputSelect.find(":selected").text();
+            varParseo = varParseo.split(",");
+            varParseo = varParseo[2];
+            varParseo = varParseo.split(" ");
+            varParseo = varParseo[2];
+            let precioProducto = parseInt(varParseo);
+            let precioFinalTupla = precioProducto * cantidad
+            console.log("El precio de la tupla completa es: %s",precioFinalTupla);
+            let inputTotal=$(`#id_total`);
+            let precioTotal = parseInt(inputTotal[0].value);
+            acumulador +=precioFinalTupla;
+            console.log(precioTotal);
+          }else{
+            console.log("No se puede calcular el precio, falta llenar algún campo.");
+          }
         }
       }
       document.getElementById("id_total").value = acumulador;
@@ -71,11 +70,12 @@
   			$("#id_form-TOTAL_FORMS").val(totalTuplas);
         let $field = $(field);
         $buttons.before($field);
-        let cantidad =$(`#id_form-${id}-cantidad`, $field).on("input", function() { obtenerTotal(); });//Obtengo la cantidad del elemento recien agregado
+        let cantidad =$(`#id_form-${id}-cantidad`, $field).on("input", function() { calcularTotal(); });//Obtengo la cantidad del elemento recien agregado
         let producto = document.querySelector(`select[name="form-${id}-producto"]`, $field);
         producto.onchange=function(e) {
-          obtenerTotal();
+          calcularTotal();
         };
+        let borrar = $(`#id_form-${id}-DELETE`, $field).on("change", function() { calcularTotal(); });
   		} else{ console.log("Número máximo de productos alcanzado. NUMERO MAXIMO = %d",totalTuplas)}
   		console.log("ADD - Total al finalizar = %d",totalTuplas);
   	}
