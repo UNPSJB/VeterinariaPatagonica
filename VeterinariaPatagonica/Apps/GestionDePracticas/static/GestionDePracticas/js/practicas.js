022f63a728d0599fac7988055f91ca238f18611b
LucilaAllende <abigail_lucii@hotmail.com>
2018-11-18 12:01:50 -0300

    Agregando calendario a Cirugias

--- VeterinariaPatagonica/Apps/GestionDePracticas/static/GestionDePracticas/js/practicas.js
+++ VeterinariaPatagonica/Apps/GestionDePracticas/static/GestionDePracticas/js/practicas.js
@@ -324,3 +324,14 @@ function verAgenda(){
 
 $("#id_inicio").after(agenda);
 $("#id_inicio").on("change", verAgenda);
+
+$(function () {
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes();
+        var dateTime = date+' '+time;
+        $("#id_inicio").datetimepicker({
+            locale: 'es',
+        });
+
+});
