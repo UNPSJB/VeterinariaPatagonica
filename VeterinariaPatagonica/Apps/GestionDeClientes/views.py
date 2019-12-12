ffa26ccf7a40fefb37b2af485e505335e99022da
lucila allende <abigail_lucii@hotmail.com>
2019-12-12 09:56:25 -0300

    Corrigiendo ayuda de cliente

--- VeterinariaPatagonica/Apps/GestionDeClientes/views.py
+++ VeterinariaPatagonica/Apps/GestionDeClientes/views.py
@@ -274,32 +274,12 @@ def tabla(pdf, y, clientes):
     # Definimos la coordenada donde se dibujará la tabla
     detalle_orden.drawOn(pdf, 20, y)
 
+@login_required
 def ayudaContextualCliente(request):
-    # Redireccionamos la ayuda contextual
-    return render(request, "GestionDeClientes/ayudaContextualCliente.html")
-
-'''@login_required
-def documentationCliente(request, tipo):
-    if (tipo==1):
-        template = loader.get_template('GestionDeClientes/manual_ayuda_cliente/build/archivos/botonHD.html')
-    elif(tipo==2):
-        template = loader.get_template('GestionDeClientes/manual_ayuda_cliente/build/archivos/criteriosBusqueda.html')
-
+# Redireccionamos la ayuda contextual
+    template = loader.get_template('GestionDeClientes/ayudaContextualCliente.html')
     contexto = {
-        'clientes': clientes,
         'usuario': request.user,
     }
 
     return HttpResponse(template.render(contexto, request))
-
-@login_required
-def documentation(request):
-
-    template = loader.get_template('GestionDeClientes/manual_ayuda_cliente/build/index.html')
-    contexto = {
-        'clientes': clientes,
-        'usuario': request.user,
-    }
-
-    return HttpResponse(template.render(contexto, request))'''
-
